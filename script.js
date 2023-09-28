2bbb145fd21bf6c09511b8fb7554cdb231d718e8
Rohit-Rajvanshi <ryugarohit123@gmail.com>
2023-09-28 20:03:05 +0530

    90%

--- script.js
+++ script.js
@@ -16,16 +16,17 @@ function createGrid(size) {
 
         gridContainer.appendChild(gridRow);
     }
-   document.getElementById("size-value").innerHTML=gridSize.value;
+    document.getElementById("size-value").innerHTML=gridSize.value;
 
-const gridCells=document.querySelectorAll(".grid-cell")
-gridCells.forEach((gridCell) => {
-gridCell.addEventListener("mouseover", () => {
-    gridCell.style.backgroundColor = color.value;
-});
-});
+    const gridCells=document.querySelectorAll(".grid-cell")
+    gridCells.forEach((gridCell) => {
+        gridCell.addEventListener("mouseover", () => {
+            gridCell.style.backgroundColor = color.value;
+        });
+        });
 
-const reset=document.querySelector(".reset")
+
+    const reset=document.querySelector(".reset")
     reset.addEventListener("click", () => {
          gridCells.forEach((gridCell) => {
         gridCell.style.backgroundColor = "white";
@@ -43,4 +44,43 @@ gridSize.addEventListener("input", () => {
    document.getElementById("size-value").innerHTML = gridSize.value ;
 });
 
-    
+let rainbowActive = false; // Variable to track if rainbow mode is active
+
+function rainbow() {
+    const array = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+    let arrayIndex = 0;
+    const gridCells = document.querySelectorAll(".grid-cell");
+
+    // Remove the color mode event listeners (if any)
+    gridCells.forEach((gridCell) => {
+        gridCell.removeEventListener("mouseover", colormode);
+    });
+
+    gridCells.forEach((gridCell) => {
+        gridCell.addEventListener("mousemove", () => {
+            if (rainbowActive) {
+                gridCell.style.backgroundColor = array[arrayIndex];
+                arrayIndex = (arrayIndex + 1) % array.length;
+            }
+        });
+    });
+
+    rainbowActive = true;
+}
+
+function colormode() {
+    const gridCells = document.querySelectorAll(".grid-cell");
+
+    // Remove the rainbow event listeners (if any)
+    gridCells.forEach((gridCell) => {
+        gridCell.removeEventListener("mousemove", rainbow);
+    });
+
+    gridCells.forEach((gridCell) => {
+        gridCell.addEventListener("mouseover", () => {
+            gridCell.style.backgroundColor = color.value;
+        });
+    });
+
+    rainbowActive = false;
+}
